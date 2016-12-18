a666176fbd217fe9ffdd9fe2d3174fe31f35b3b0
xujingguo <xujingguo58@outlook.com>
2016-12-18 11:15:13 +0800

    https://github.com/xujingguo58/TinyCLangCompiler.git

---
+++ TinyCLangComplier/TinyCLangComplier.cpp
@@ -0,0 +1,17 @@
+// TinyCLangComplier.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
+#include<iostream>
+
+using namespace std;
+int main(){
+	cout << "hello world!";
+}
